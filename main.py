d959aed47aa33f0f1d39474d49a7d23d752b9be6
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-29 15:42:52 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ from typing import Text
 import khayyam
 import telebot
 from telebot.types import Message
-
+import math
 mybot = telebot.TeleBot("2028397609:AAHTTHWu45NmwEX8o9dFKg1YTICcYqGIJUg")
-number =random.randint(0,20)
-# mymarkup = telebot.types.ReplyKeyboardMarkup(row_width=3)
+number =random.randint(1,20)
+
 
 mybutten = telebot.types.ReplyKeyboardMarkup(row_width= 1)
-# btn1 =  telebot.types.KeyboardButton("muzic")
-# btn2 = telebot.types.KeyboardButton("video")
-# btn3 = telebot.types.KeyboardButton("pic")
 
-# mymarkup.add(btn1,btn2,btn3)
 btn1 = telebot.types.KeyboardButton("new game")
+
 mybutten.add(btn1)
 
 @mybot.message_handler(commands=["start"])
@@ -26,90 +23,91 @@ def send_welcome(message):
     mybot.reply_to(message,f"Hi  {message.from_user.first_name} خوش آمدید \n /help انتخاب کن تا کمکت کنم" )
 
 
+@mybot.message_handler(commands=["help"])
 def help(message):
-    mybot.reply_to(message, "کمک میخوای؟ من برای کمک به شما آماده ام \
+    mybot.reply_to(message ,            "کمک میخوای؟ من برای کمک به شما آماده ام \
                             \n  /game اگر دوست داری با هم بازی حدس عدد شروع کنیم \
                             \n  /age  تاریخ تولدت وارد کن تا من بگم چندسال هست داری اکسیژن مصرف می کنی  \
-                            \n  / voice یه جمله به انگلیسی تایپ کن من جمله رو برات بخونم \
+                            \n  /voice یه جمله به انگلیسی تایپ کن من جمله رو برات بخونم \
                             \n /qrcode مشخصات یه محصول وارد کن تا من کیو آرکدش بگم \
                             \n /max چند تا عدد وارد کن من بزرگترین عدد برات اعلام کنم \
                             \n /argmax چندتا عدد وارد کن من اندیس بزرگترین عدد نمایش بدم ")
 
-
-@mybot.message_handler(commands=["help"])
-def helpyou(message):
-    mybot.reply_to(message , "لطفا یکی از گزینه ها راوارد کنید \
-                             \n /help به شما اطلاعات می دهم")
-
+# help(Message)
 
 @mybot.message_handler(commands=["game"])
 def game(message ):
-     mybot.reply_to(message , "اومدیم با هم بازی حدس اعداد انجام بدیم؟ بزن بریم" )
-    #  number =random.randint(0,20)
-# @mybot.message_handler(commands=["Game"])
+     play= mybot.reply_to(message , "اومدیم با هم بازی حدس اعداد انجام بدیم؟ یه عددبین 1 تا 20 انتخاب کن و بزن بریم" )
+     mybot.register_next_step_handler(play , new_game )
+    
 
-@mybot.message_handler( func=lambda  message:True)
+# @mybot.message_handler( func=lambda  message:True)
 def new_game(message):
     global number
     if int(message.text) == number:
-        mybot.reply_to(message ,"dorost hads zadi")
+        mybot.reply_to(message ,"درست حدس زدی.آفرین")
         global game
         
     elif int(message.text ) > number:
-        mybot.reply_to(message ,"boro paeen")
+        mybot.reply_to(message ,"برو پایین")
 
 
     elif int(message.text) < number :
-        mybot.reply_to(message ,"boro bala")
+        mybot.reply_to(message ,"برو بالا")
+
+    # else: 
+    #     mybot.send_message(message , " دفت کن و عدد درست انتخاب کن")
 
 @mybot.message_handler(commands=["age"])
 def enter_ege(message):
-    mybot.reply_to(message ,"enter your date birthday (yyyy / mm / dd): ")
-
+    date = mybot.reply_to(message ,"تاریخ تولدت وارد کن به صورت yyyy/mm/dd ")
+    mybot.register_next_step_handler(date , Age)
     
-@mybot.message_handler(func= lambda message:True)
+# @mybot.message_handler(func= lambda message:True)
 def Age(message):
-    print(JalaliDate.message.todate())
+    mybot.reply_to(JalaliDate.todate())
 
 
 @mybot.message_handler(commands=["voice"])
 def sentences(message):
-    mybot.reply_to(message,"enter your sentences:")
+    voice = mybot.reply_to(message,"یک جمله به لاتین وارد کن:")
+    mybot.register_next_step_handler(voice ,Voice )
 
-mybot.message_handler(func= lambda message:True)
-def voice(message):
-    mytext = message
+def Voice(message):
+    mytext = message.text
     language = "en"
-    myobj = gTTS(text=mytext,lang=language ,slow=False)
-    myobj.save("voice.mp3")
+    my_voice = gTTS(text=mytext,lang=language ,slow=False)
+    my_voice.save("voice.mp3")
+  
 
+@mybot.message_handler(commands=["qrcode"])
+def Code(message):
+    rcode = mybot.reply_to(message , "جمله ی خود را وارد کنید :")
+    mybot.register_next_step_handler(rcode , qrcode)
 
-@mybot.message_handler(commands=["download"])
-def my_down(message):
-    mybot.reply_to(message, "kodom mikhay dhnlod koni", reply_markup =mymarkup )
+def qrcode(message):
+    text = message
+    image = qrcode.make(text)
+    image.save("qrcode.png")
 
+@mybot.message_handler(commands=["max"])
+def araye(message):
+    array = mybot.reply_to(message , " چند عدد وارد کنید وبعد از هرکدام ،بگذارید.تا بزرگترین عدد اعلام کنم")
+    mybot.register_next_step_handler(array , Max)
 
-# @mybot.message_handler(commands=["fal"])
-# def send_fal(message):
-#     falha = ["به سفر خواهی رفت","به دیدار معشوق خواهی رفت", "به فناخواهی رفت"]
-#     selected_fal= random.choice(falha)
-#     mybot.reply_to(message , selected_fal)
+def Max(message):
+    arrayeh = []
+    arrayeh.append(arrayeh)
+    m = max(arrayeh)
+    
+@mybot.message_handler(commands=["argmax"])
+def arg(message):
+    andis = mybot.reply_to(message , " چند عدد وارد کنید وبعد از هرکدام ، بگذارید.تا بزرگترین اندیس عدد اعلام کنم")
+    mybot.register_next_step_handler(andis , Argmax)
 
-# @mybot.message_handler(func=lambda message:True)
-# def my_function_3(message):
-#     if message.text == "salam":
-#         mybot.reply_to(message ,"hello")
-#     elif message.text == "fine?":
-#         mybot.reply_to(message , "چطوری ؟")
-#     elif message.text == "bos":
-#         mybot.reply_to(message ,"boss")
-#     elif message.text =="chi poshidi":
-#         photo = open("saeidhe.jpg", "rb")
-#         mybot.send_photo(message.chat.id, photo)
-
-#     else:
-#         # mybot.reply_to(message,"nemifahmam chi migi")
-#         mybot.send_message(message.chat.id,"nemifahmam chi migi")
+def Argmax(message):
+    argm = []
+    m = max(argm[0])
 
 
 mybot.polling()
